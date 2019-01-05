f7357b5cdcaccb6660f1a8684d41f8c675a9aa41
Cory Norris <corynorris@gmail.com>
2019-01-05 12:05:10 -0500

    added react

---
+++ assets/js/components/Home.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <section className="phx-hero">
+          <h1>Welcome to Phoenix</h1>
+          <p>
+            A productive web framework that
+            <br />
+            does not compromise speed and maintainability.
+          </p>
+        </section>
+
+        <section className="row">
+          <article className="column">
+            <h2>Resources</h2>
+            <ul>
+              <li>
+                <a href="https://hexdocs.pm/phoenix/overview.html">
+                  Guides &amp; Docs
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/phoenixframework/phoenix">Source</a>
+              </li>
+              <li>
+                <a href="https://github.com/phoenixframework/phoenix/blob/v1.4/CHANGELOG.md">
+                  v1.4 Changelog
+                </a>
+              </li>
+            </ul>
+          </article>
+          <article className="column">
+            <h2>Help</h2>
+            <ul>
+              <li>
+                <a href="https://elixirforum.com/c/phoenix-forum">Forum</a>
+              </li>
+              <li>
+                <a href="https://webchat.freenode.net/?channels=elixir-lang">
+                  #elixir-lang on Freenode IRC
+                </a>
+              </li>
+              <li>
+                <a href="https://twitter.com/elixirphoenix">
+                  Twitter @elixirphoenix
+                </a>
+              </li>
+            </ul>
+          </article>
+        </section>
+      </div>
+    );
+  }
+}
