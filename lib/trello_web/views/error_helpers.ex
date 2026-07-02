6e63db952a34bc41a766b112e0ffaa184cf28ee3
corynorris <corynorris@gmail.com>
2026-07-01 22:28:03 -0400

    fix: replace use Phoenix.HTML with import Phoenix.HTML for phoenix_html v4 compat

--- lib/trello_web/views/error_helpers.ex
+++ lib/trello_web/views/error_helpers.ex
@@ -3,7 +3,7 @@ defmodule TrelloWeb.ErrorHelpers do
   Conveniences for translating and building error messages.
   """
 
-  use Phoenix.HTML
+  import Phoenix.HTML
 
   @doc """
   Generates tag for inlined form input errors.
